df829ff574b6f4d99e8d5253d3c821a55200a47e
Ajay Tiwari <76895158+maggi121997@users.noreply.github.com>
2021-03-05 01:03:47 +0530

    Initial commit

---
+++ src/Home_content.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home_content.css";
+
+export default function HomeContent(){
+return(
+  <div className="home_content">
+    <div className="plant_img">
+       <div className="plant_pic"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQil7QcHlJnGRgtdJfMLPh9_H_6-WrvfVMnkw&usqp=CAU"/> </div>
+       <div className="plant_intro">
+         <h3> What is Lorem Ipsum ? <br/><br/> <p> "Lorem ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text" </p> </h3>
+         <img src="https://nasdonline.org/static_content/documents/1901/01.jpg" />
+    </div>
+  </div>
+
+
+
+  </div>
+);
+}
